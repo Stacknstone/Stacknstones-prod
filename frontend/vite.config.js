4f8b05cd1d215450865e1e7234e61784d5096f17
Stacknstone <stacknstones.construct@gmail.com>
2025-11-09 08:01:04 +0000

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   // This is the line that fixes the 404 errors
-  base: '/Stacknstones-prototype/', 
+  base: '/', 
   plugins: [react()],
 })
